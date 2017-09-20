601c478cae9a3cc7959eaeeb4afa0619c52e4969
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-20 12:33:22 -0400

    begin adding Signup comp

--- client/main.js
+++ client/main.js
@@ -1,3 +1,13 @@
 import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class Signup extends React.Component {
+  render() {
+    return <p> Signup Component</p>;
+  }
+}
+
+Meteor.startup(() => {
+  ReactDOM.render(<Signup/>, document.getElementById('app'));
+});
