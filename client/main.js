cc4872267f9c8d96825bfde04cf95139308fdadc
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-20 12:50:45 -0400

    created Link component

--- client/main.js
+++ client/main.js
@@ -2,8 +2,9 @@ import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Signup from '../imports/ui/Signup';
+import Link from '../imports/ui/Link';
 
 
 Meteor.startup(() => {
-  ReactDOM.render(<Signup/>, document.getElementById('app'));
+  ReactDOM.render(<Link/>, document.getElementById('app'));
 });
