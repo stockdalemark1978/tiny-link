55c1b1e1c7c6f1b85561c8fef7dd155f02826725
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-20 18:52:22 -0400

    added signup and link routes

--- client/main.js
+++ client/main.js
@@ -1,10 +1,20 @@
 import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
 
 
+//router config
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/signup" component={Signup}/>
+    <Route path="/link" component={Link}/>
+  </Router>
+);
+
+
 Meteor.startup(() => {
-  ReactDOM.render(<Link/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
