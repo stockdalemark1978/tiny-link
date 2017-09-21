dbc11da3f811eb2dcd30a363dab6c47a8bc26908
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-21 12:14:03 -0400

    added Link component on Login component with route to signup

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Login extends React.Component {
   render() {
-    return <p>Login component</p>;
+    return (
+      <div>
+        <h1>Login to Tiny Link</h1>
+
+        <Link to="/signup">Already have an account?</Link>
+      </div>
+    )
   }
 }
