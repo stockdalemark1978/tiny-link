ac1886d8f3cde996d67c61d4af4d8c50b8a22a93
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-21 11:38:20 -0400

    added Login component

--- client/main.js
+++ client/main.js
@@ -1,15 +1,17 @@
-import {Meteor} from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
 import NotFound from '../imports/ui/NotFound';
+import Login from '../imports/ui/Login';
 
 
 //router config
 const routes = (
   <Router history={browserHistory}>
+    <Route path="/" component={Login}/>
     <Route path="/signup" component={Signup}/>
     <Route path="/link" component={Link}/>
     <Route path="*" component={NotFound}/>
