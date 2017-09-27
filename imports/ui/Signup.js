423d653d75261ccfb4e42200de6db6fd57bd2966
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-27 15:37:43 -0400

    added signup form to Signup component

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -5,28 +5,18 @@ export default class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      error: ''
     };
-  }
-  increment() {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-
-  decrement() {
-    this.setState({
-      count: this.state.count - 1
-    })
   }
   render() {
     return (
       <div>
         <h1>Signup to Tiny Link</h1>
-
-        <p>{this.state.count}</p>
-        <button onClick={this.increment.bind(this)}>+1</button>
-        <button onClick={this.decrement.bind(this)}>-1</button>
+        <form>
+          <input type="email" name="email" placeholder="Please enter a valid email address"/>
+          <input type="password" name="password" placeholder="Password"/>
+          <button>Create Account</button>
+        </form>
 
         <Link to="/">Already have an account? Login here.</Link>
       </div>
