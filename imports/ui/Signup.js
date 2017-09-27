cc5777e9c25d2315693cc6960c447a345c4f67de
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-27 15:29:05 -0400

    adding react state

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
   render() {
     return (
       <div>
         <h1>Signup to Tiny Link</h1>
+
+        <p>{this.state.count}</p>
+        <button onClick={this.increment.bind(this)}>+1</button>
 
         <Link to="/">Already have an account? Login here.</Link>
       </div>
