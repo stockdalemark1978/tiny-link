9a85e2d101a1cfe082dc461d1d98350ccbb62767
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-09-28 15:17:11 -0400

    building Login form and function

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -2,10 +2,25 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  }
   render() {
     return (
       <div>
         <h1>Login to Tiny Link</h1>
+
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Please enter a valid email address"/>
+          <input type="password" ref="password" name="password" placeholder="Password"/>
+          <button>Login</button>
+        </form>
+
 
         <Link to="/signup">Don't have an account yet? Signup here.</Link>
       </div>
