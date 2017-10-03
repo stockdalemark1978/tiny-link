3fcec5b35de35c8a7673c0f06275210c390835c1
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-10-03 14:57:51 -0400

    added Signup error message

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -16,12 +16,13 @@ export default class Signup extends React.Component {
     let password = this.refs.password.value.trim();
 
     Accounts.createUser({email, password}, (err) => {
-        console.log('Login!!!', err);
+      if (err) {
+        this.setState({error: err.reason})
+      } else {
+        this.setState({error: ''});
+      }
     });
 
-    // this.setState({
-    //   error: "Incorrect Value"
-    // });
   }
   render() {
     return (
