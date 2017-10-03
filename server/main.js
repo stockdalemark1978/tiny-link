f453fc1b023aa97864dfeb470e1c340eb5cfa04c
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-10-03 16:56:51 -0400

    playing with simpl-schema settings

--- server/main.js
+++ server/main.js
@@ -1,5 +1,30 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  const employeeSchema = new SimpleSchema ({
+    name: {
+      type: String,
+      min: 1,
+      max: 200
+    },
+    hourlyWage: {
+      type: Number,
+      min: 0
+    },
+    email: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email
+    }
+
+});
+
+  employeeSchema.validate({
+    name: "",
+    hourlyWage: 4,
+    email: "stockdalemark@gmail.com"
+  });
+
 });
