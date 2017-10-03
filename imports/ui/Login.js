b6bf747d2daba527ef8bbc13369b62ec144b7db4
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-10-03 15:00:15 -0400

    added Login error message

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -16,7 +16,11 @@ export default class Login extends React.Component {
     let password = this.refs.password.value.trim();
 
     Meteor.loginWithPassword({email}, password, (err) => {
-      console.log('Login!!!', err);
+      if (err) {
+        this.setState({error: err.reason})
+      } else {
+        this.setState({error: ''});
+      };
     });
 
   }
