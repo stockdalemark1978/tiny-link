366f610eb3528247c06f4c90b982f16b1c944c9e
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-10-04 20:42:00 -0400

    import Accounts to main.js and add validate method

--- server/main.js
+++ server/main.js
@@ -3,28 +3,28 @@ import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
-
-  const employeeSchema = new SimpleSchema ({
-    name: {
-      type: String,
-      min: 1,
-      max: 200
-    },
-    hourlyWage: {
-      type: Number,
-      min: 0
-    },
-    email: {
-      type: String,
-      regEx: SimpleSchema.RegEx.Email
-    }
-
-});
-
-  employeeSchema.validate({
-    name: "Larry",
-    hourlyWage: 4,
-    email: "stockdalemark@gmail.com"
-  });
+//
+//   const employeeSchema = new SimpleSchema ({
+//     name: {
+//       type: String,
+//       min: 1,
+//       max: 200
+//     },
+//     hourlyWage: {
+//       type: Number,
+//       min: 0
+//     },
+//     email: {
+//       type: String,
+//       regEx: SimpleSchema.RegEx.Email
+//     }
+//
+// });
+//
+//   employeeSchema.validate({
+//     name: "Larry",
+//     hourlyWage: 4,
+//     email: "stockdalemark@gmail.com"
+//   });
 
 });
