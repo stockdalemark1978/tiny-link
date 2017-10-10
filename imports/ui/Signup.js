44d6b72b9c62875c88f67b77e3783bbabb22458d
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-10-09 20:14:00 -0400

    adding password length validation

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -15,6 +15,10 @@ export default class Signup extends React.Component {
     let email = this.refs.email.value.trim();
     let password = this.refs.password.value.trim();
 
+    if (password.length < 9) {
+      return this.setState({error: 'Password must be more than 8 characters long'});
+    }
+
     Accounts.createUser({email, password}, (err) => {
       if (err) {
         this.setState({error: err.reason})
