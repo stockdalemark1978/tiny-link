b4a7031c5bf583b22142278c09b52c997f4a9961
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-10-21 11:59:47 -0400

    setting up LinksList comp

---
+++ imports/ui/LinksList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class LinksList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Links List</p>
+      </div>
+    )
+  }
+};
